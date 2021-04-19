4e714f8a9d0c960c048d70ffb9c9eabea8ab2909
hkjpotato <hkjpotato@gmail.com>
2021-04-18 20:18:34 -0400

    so called functional component vs native tag

--- src/index.js
+++ src/index.js
@@ -3,8 +3,26 @@
 
 import { React, ReactDOM } from './MyReact';
 
-ReactDOM.render(
-  <div>hello</div>,
-  document.getElementById('root')
-);
+
+function Hello() {
+  return <div><a /></div>;
+  // essentially it is function Hello() { return React.createElement('div', , React.createElement('a'))}
+}
+
+const element = <Hello />;
+
+window.ele = element;
+console.log(element);
+// ReactDOM.render(
+//   (
+//     <div>
+//       <ol>
+//         <li></li>
+//         <li></li>
+//       </ol>
+//       <span></span>
+//     </div>
+//   ),
+//   document.getElementById('root')
+// );
 
